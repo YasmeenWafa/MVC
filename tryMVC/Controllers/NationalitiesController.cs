5f18a63e4ff2bd613a0a31f9a971c57cf8fc6fab
YasmeenWafa <yasmeen_wafa@hotmail.com>
2016-08-12 23:56:14 +0200

    finished work model, home page needs some beautifying :)

--- tryMVC/Controllers/NationalitiesController.cs
+++ tryMVC/Controllers/NationalitiesController.cs
@@ -66,36 +66,7 @@ namespace tryMVC.Controllers
             return View(nationalitiesModel);
         }
 
-        // GET: Nationalities/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            NationalitiesModel nationalitiesModel = db.Nationalities.Find(id);
-            if (nationalitiesModel == null)
-            {
-                return HttpNotFound();
-            }
-            return View(nationalitiesModel);
-        }
-
-        // POST: Nationalities/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "nationalityID,nationalityName")] NationalitiesModel nationalitiesModel)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(nationalitiesModel).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(nationalitiesModel);
-        }
+       
 
         // GET: Nationalities/Delete/5
         public ActionResult Delete(int? id)
